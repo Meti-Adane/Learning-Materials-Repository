51731952a61ed9008f5e4ca2e8f6ee0a66bc60be
Mastewal B <se.mastewal.belete@gmail.com>
2021-06-15 14:37:33 +0300

    Configurations

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
+import os
+
 class Config(object):
     DEBUG = False
     TESTING = False
+    JWT_SECRET_KEY = 'super-secret-key'
+    JWT_BLACKLIST_ENABLED = False
+    JWT_BLACKLIST_TOKEN_CHECKS = ['access']
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ProductionConfig(Config):
@@ -8,10 +14,16 @@ class ProductionConfig(Config):
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = <Development DB URL>
+    #SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_ECHO = False
 
 class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = "<Testing DB URL>"
     SQLALCHEMY_ECHO = False
+
+app_config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig
+}
