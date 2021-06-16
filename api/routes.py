1f1459b6c522ef92bc2359413663d847136afd9c
Mastewal B <se.mastewal.belete@gmail.com>
2021-06-16 18:51:28 +0300

    Register Login and Logout

--- api/routes.py
+++ api/routes.py
@@ -1,10 +1,13 @@
 from flask import Blueprint
 from flask_restful import Api
 from api.books.books import GetBooks, GetBook, DownloadBook
+from api.admin.adminstrator import AddBook
 from api.courses.courses import GetCourses, GetCourse
+from api.auth.authenticate import Register, Login, Logout, ResetPassword
 
 mod = Blueprint('api', __name__)
 api = Api(mod)
+
 
 
 api.add_resource(GetBooks, '/api/books')
@@ -12,3 +15,10 @@ api.add_resource(GetBook, '/api/book/<book_id>')
 api.add_resource(DownloadBook, '/api/book/<book_id>/download')
 api.add_resource(GetCourses, '/api/courses')
 api.add_resource(GetCourse, '/api/course/<course_id>')
+
+api.add_resource(AddBook, '/api/books')
+
+api.add_resource(Register, '/api/auth/register')
+api.add_resource(Login, '/api/auth/login')
+api.add_resource(Logout, '/api/auth/logout')
+api.add_resource(ResetPassword, '/api/auth/reset-password')
