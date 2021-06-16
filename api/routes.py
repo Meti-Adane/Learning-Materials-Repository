4de778353e143d01ed11df5dcca31c712a2596aa
Mastewal B <se.mastewal.belete@gmail.com>
2021-06-16 07:32:46 +0300

    download route

--- api/routes.py
+++ api/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask_restful import Api
-from api.books.books import GetBooks, GetBook
+from api.books.books import GetBooks, GetBook, DownloadBook
 from api.courses.courses import GetCourses, GetCourse
 
 mod = Blueprint('api', __name__)
@@ -9,5 +9,6 @@ api = Api(mod)
 
 api.add_resource(GetBooks, '/api/books')
 api.add_resource(GetBook, '/api/book/<book_id>')
+api.add_resource(DownloadBook, '/api/book/<book_id>/download')
 api.add_resource(GetCourses, '/api/courses')
 api.add_resource(GetCourse, '/api/course/<course_id>')
