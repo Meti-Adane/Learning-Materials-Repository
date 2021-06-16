e45bd2d16ec13f36435166e74afdbb84dde5b2c5
Mastewal B <se.mastewal.belete@gmail.com>
2021-06-16 08:43:04 +0300

    validation schema for book

--- api/admin/validate.py
+++ api/admin/validate.py
@@ -8,3 +8,50 @@ def validate_arg(arg):
         arg = int(arg)
     except Exception as e:
         return Response(json.dumps({"Message":"Invalid argument passed"}), status=400)
+
+
+def valdiate_book(data):
+    validate_schema = {
+        'title': {
+            'type': 'string',
+            'required': True,
+            'empty': False,
+            'maxlength': 500 
+        },
+
+        'author':{
+            'type': 'string',
+            'required': True,
+            'empty': False,
+            'maxlength': 100
+        },
+        'isbn': {
+            'type': 'string',
+            'required': True,
+            'empty': False,
+            'maxlength': 100
+        },
+        'publisher': {
+            'type': 'string',
+            'empty': False,
+            'maxlength': 100
+        },
+        'description':{
+            'type':'string'
+        },
+        'publish_date':{
+            
+        },
+        'edition':{
+
+        },
+        'image_file':{
+
+        },
+        'skill_level':{
+
+        },
+        'book_file':{
+
+        }
+    }
