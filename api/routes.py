997d614da38d4de58080d2a9e39ab5bed35f55b0
Mastewal B <se.mastewal.belete@gmail.com>
2021-06-16 06:41:39 +0300

    bug fix

--- api/routes.py
+++ api/routes.py
@@ -7,7 +7,7 @@ mod = Blueprint('api', __name__)
 api = Api(mod)
 
 
-api.add_resource(GetBooks, 'api/books')
-api.add_resource(GetBook, 'api/book/<book_id>')
-api.add_resource(GetCourses, 'api/courses')
-api.add_resource(GetCourse, 'api/course/<course_id>')
+api.add_resource(GetBooks, '/api/books')
+api.add_resource(GetBook, '/api/book/<book_id>')
+api.add_resource(GetCourses, '/api/courses')
+api.add_resource(GetCourse, '/api/course/<course_id>')
